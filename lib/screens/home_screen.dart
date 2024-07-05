ddf99e49aa5da41e9a83fcf3147bedd601e12198
ssaakaash <ssaakaash@icloud.com>
2024-07-05 12:38:09 +0530

    feat: add app drawer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -18,6 +18,75 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
+      drawer: Drawer(
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            Container(child: const SizedBox(height: 100,), color: Colors.transparent),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: ListTile(
+                leading: Icon(Icons.home_outlined, size: 36),
+                title: Center(child: Text('Home', style: TextStyle(fontSize: 28))),
+                onTap: () {
+                  // Handle the tap
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: ListTile(
+                leading: Icon(Icons.search_outlined, size: 36),
+                title: Center(child: Text('Search', style: TextStyle(fontSize: 28))),
+                onTap: () {
+                  // Handle the tap
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: ListTile(
+                leading: Icon(Icons.grid_view_outlined, size: 36),
+                title: Center(child: Text('Your Events', style: TextStyle(fontSize: 28))),
+                onTap: () {
+                  // Handle the tap
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: ListTile(
+                leading: Icon(Icons.account_circle_outlined, size: 36),
+                title: Center(child: Text('Your Profile', style: TextStyle(fontSize: 28))),
+                onTap: () {
+                  // Handle the tap
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: ListTile(
+                leading: Icon(Icons.settings_outlined, size: 36),
+                title: Center(child: Text('Settings', style: TextStyle(fontSize: 28))),
+                onTap: () {
+                  // Handle the tap
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: ListTile(
+                leading: Icon(Icons.queue_music_outlined, size: 36),
+                title: Center(child: Text('Your Playlists', style: TextStyle(fontSize: 28))),
+                onTap: () {
+                  // Handle the tap
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -9,7 +9,7 @@ final ThemeData darkTheme = ThemeData(
   ),
   scaffoldBackgroundColor: const Color(0xFF121212),
   appBarTheme: const AppBarTheme(
-    backgroundColor: Color.fromRGBO(255, 255, 255, 0.05),
+    backgroundColor: Color(0xff1d1d1d),
   ),
   inputDecorationTheme: const InputDecorationTheme(
     filled: true,
