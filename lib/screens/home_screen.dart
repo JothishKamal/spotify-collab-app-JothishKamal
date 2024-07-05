ce7d7964fed3836a6cb4e5b9592013de3e4ebaab
ssaakaash <ssaakaash@icloud.com>
2024-07-05 13:13:27 +0530

    feat: add gradient to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -23,12 +23,17 @@ class HomeScreen extends StatelessWidget {
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
-            Container(child: const SizedBox(height: 100,), color: Colors.transparent),
+            Container(
+                child: const SizedBox(
+                  height: 100,
+                ),
+                color: Colors.transparent),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 10),
               child: ListTile(
                 leading: Icon(Icons.home_outlined, size: 36),
-                title: Center(child: Text('Home', style: TextStyle(fontSize: 28))),
+                title:
+                    Center(child: Text('Home', style: TextStyle(fontSize: 28))),
                 onTap: () {
                   // Handle the tap
                 },
@@ -38,7 +43,8 @@ class HomeScreen extends StatelessWidget {
               padding: const EdgeInsets.symmetric(vertical: 10),
               child: ListTile(
                 leading: Icon(Icons.search_outlined, size: 36),
-                title: Center(child: Text('Search', style: TextStyle(fontSize: 28))),
+                title: Center(
+                    child: Text('Search', style: TextStyle(fontSize: 28))),
                 onTap: () {
                   // Handle the tap
                 },
@@ -48,7 +54,8 @@ class HomeScreen extends StatelessWidget {
               padding: const EdgeInsets.symmetric(vertical: 10),
               child: ListTile(
                 leading: Icon(Icons.grid_view_outlined, size: 36),
-                title: Center(child: Text('Your Events', style: TextStyle(fontSize: 28))),
+                title: Center(
+                    child: Text('Your Events', style: TextStyle(fontSize: 28))),
                 onTap: () {
                   // Handle the tap
                 },
@@ -58,7 +65,9 @@ class HomeScreen extends StatelessWidget {
               padding: const EdgeInsets.symmetric(vertical: 10),
               child: ListTile(
                 leading: Icon(Icons.account_circle_outlined, size: 36),
-                title: Center(child: Text('Your Profile', style: TextStyle(fontSize: 28))),
+                title: Center(
+                    child:
+                        Text('Your Profile', style: TextStyle(fontSize: 28))),
                 onTap: () {
                   // Handle the tap
                 },
@@ -68,7 +77,8 @@ class HomeScreen extends StatelessWidget {
               padding: const EdgeInsets.symmetric(vertical: 10),
               child: ListTile(
                 leading: Icon(Icons.settings_outlined, size: 36),
-                title: Center(child: Text('Settings', style: TextStyle(fontSize: 28))),
+                title: Center(
+                    child: Text('Settings', style: TextStyle(fontSize: 28))),
                 onTap: () {
                   // Handle the tap
                 },
@@ -78,13 +88,35 @@ class HomeScreen extends StatelessWidget {
               padding: const EdgeInsets.symmetric(vertical: 10),
               child: ListTile(
                 leading: Icon(Icons.queue_music_outlined, size: 36),
-                title: Center(child: Text('Your Playlists', style: TextStyle(fontSize: 28))),
+                title: Center(
+                    child:
+                        Text('Your Playlists', style: TextStyle(fontSize: 28))),
                 onTap: () {
                   // Handle the tap
                 },
               ),
             ),
           ],
+        ),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment(0.8, 1),
+            colors: <Color>[
+              Color(0xff000000),
+              Color(0xff051e0f),
+              Color(0xff083216),
+            ], // Gradient from https://learnui.design/tools/gradient-generator.html
+            tileMode: TileMode.mirror,
+          ),
+        ),
+        child: const Center(
+          child: Text(
+            'Collabify',
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
         ),
       ),
     );
